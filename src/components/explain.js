0f9bee1f69274f35c909053983ad6e6851592330
Seth Moffat <sethmoffat1@gmail.com>
2022-04-14 17:09:34 -0600

    First Commit

---
+++ src/components/explain.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Explain() {
+  return (
+
+    <div>
+        
+        <img src="https://media.discordapp.net/attachments/900157005686267954/900162870766108702/flipocrypto.png"></img>
+        <h2>Welcome to Flipocrypto.com</h2>
+        <h4>A place where smaller blockhain and crypto projects would have a chance to be shared and discussed.</h4>
+        <h5>Sadly, do to a lack of developers this project has been shut down and this web app has been converted to a static website. </h5>
+        <h5>If you are interested in purchasing the domain flipocrypto.com or you have any other questions,  </h5>
+        <h5>you can email me at <a href ="seththedeveloper@gmail.com">seththedeveloper@gmail.com</a></h5>
+        
+    </div>
+  )
+}
+
+export default Explain
